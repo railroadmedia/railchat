521fe87b71eeeca0177be07ad7d84a75a1a71896
Bogdan Damian <bogdan.damian@artsoft-consult.ro>
2021-01-26 18:58:39 +0200

    worked on basic chat functionality - WIP /1199618421923092

--- config/railchat.php
+++ config/railchat.php
@@ -1,4 +1,8 @@
 <?php
 
 return [
+    'get_stream_credentials' => [
+        'key' => env('STREAM_API_KEY'),
+        'secret' => env('STREAM_APP_SECRET'),
+    ],
 ];
--- src/Services/RailchatService.php
+++ src/Services/RailchatService.php
@@ -2,7 +2,51 @@
 
 namespace Railroad\Railchat\Services;
 
+use GetStream\StreamChat\Client;
+
 class RailchatService
 {
+    /**
+     * @var Client
+     */
+    private $client;
 
+    const ROLE_ADMINISTRATOR = 'admin';
+    const ROLE_USER = 'user';
+
+    public function __construct()
+    {
+        $credentials = config('railchat.get_stream_credentials');
+
+        $this->client = new Client($credentials['key'], $credentials['secret']);
+    }
+
+    public function updateChannel($channel)
+    {
+
+    }
+
+    public function getUserToken($userId, $displayName, $avatarUrl, $profileUrl, $isAdministrator, $channelName)
+    {
+        $userId = strval($userId);
+
+        $userData = [
+            'id' => $userId,
+            'role' => $isAdministrator ? self::ROLE_ADMINISTRATOR : self::ROLE_USER,
+            'displayName' => $displayName,
+            'avatarUrl' => $avatarUrl,
+            'profileUrl' => $profileUrl,
+        ];
+
+        $this->client->updateUser($userData);
+
+        $channel = $this->client->Channel('messaging', $channelName);
+
+        // $channel->create($userId);
+        $channel->addMembers([$userId]);
+
+        $token = $this->client->createToken($userId);
+
+        return $token;
+    }
 }
